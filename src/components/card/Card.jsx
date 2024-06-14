b5ba623ff6d8ac01bd149f77f9c0d64b4f9c234e
sohail-pixelandbeam <sohail.pixelandbeam@gmail.com>
2024-06-15 04:58:43 +0500

    add courses control functionality

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Btn from '../btn/Btn'
 
-export default function Card({name = 'default', description = 'default', img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Z-Viu23Zi9aepAkisUn3zQAUzGBys45htQ&s'  }) {
+export default function Card({name = 'default', description = 'default', img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Z-Viu23Zi9aepAkisUn3zQAUzGBys45htQ&s', showControls, onEdit, onDelete  }) {
   return (
-    <div className="course-card" data-aos="zoom-in">
+    <div className="course-card">
     <img src={img} alt="img" />
     <div className="course-card-body">
         <div className='course-card-heading' >{name}</div>
         <div className="course-card-descripiton">
             {description.slice(0, 80)} . . .
         </div>
+        {showControls ? <div className="course-card-btns-box">
+            <Btn label='Details' className="course-card-btn" style={{ backgroundColor: 'green' }} />
+            <Btn label='Edit' onClick={onEdit} className="course-card-btn" />
+            <Btn label='Delete' onClick={onDelete} className="course-card-btn" style={{background:'red'}} />
+        </div>: 
         <div className="course-card-btns-box">
-            <Btn label='Enroll Now' className="course-card-btn" style={{ backgroundColor: 'green' }} />
-            <Btn label='Check Details' className="course-card-btn" />
-        </div>
+        <Btn label='Enroll Now' className="course-card-btn" style={{ backgroundColor: 'green' }} />
+        <Btn label='Check Details' className="course-card-btn" />
+    </div>}
     </div>
 
 </div>
--- src/screens/adminPanel/adminPanelScreens/courseManagement/CourseManagement.jsx
+++ src/screens/adminPanel/adminPanelScreens/courseManagement/CourseManagement.jsx
@@ -1,49 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { API_BASE_URL, getData } from '../../../../config/apiCalls';
+import { API_BASE_URL, deleteData, getData } from '../../../../config/apiCalls';
 import Snack from '../../../../components/snack/Snack';
 import Loader from '../../../../components/loader/Loader';
-import { Grid } from '@mui/material';
+import { Grid, Modal, TextField } from '@mui/material';
 import Card from '../../../../components/card/Card';
+import Btn from '../../../../components/btn/Btn';
+import CancelIcon from '@mui/icons-material/Cancel';
+
 
 const CourseManagement = () => {
-    let [courses, setCourses] = useState([]);
-    let [isLoading, setIsLoading] = useState(false);
-    let [openSnack, setOpenSnack] = useState(false);
-    let [severity, setSeverity] = useState('error')
-    let [snackMsg, setSnackMsg] = useState('');
+  let [isAdding, setIsAdding] = useState(false);
+  let [isEditing, setIsEditing] = useState(false);
 
-    const handleCloseSnack = () => {
-        setOpenSnack(false);
-        setSnackMsg('');
-        setSeverity('error');
-    }
+  let [courses, setCourses] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
+  let [openSnack, setOpenSnack] = useState(false);
+  let [severity, setSeverity] = useState('error')
+  let [snackMsg, setSnackMsg] = useState('');
 
-    const getCourses = () => {
-        setIsLoading(true)
-        getData(`courses`).then((response) => {
-            if (response.success) {
-                console.log(response?.data)
-                setCourses(response?.data)
-                setIsLoading(false)
-            } else {
-                setSnackMsg(response.message);
-                setOpenSnack(true);
-                setIsLoading(false)
-            }
-        })
-            .catch((error) => {
-                setSnackMsg(error.message ?? "Network Error");
-                setOpenSnack(true);
-                setIsLoading(false)
-            });
-    }
+  const handleCloseSnack = () => {
+    setOpenSnack(false);
+    setSnackMsg('');
+    setSeverity('error');
+  }
 
-    useEffect(()=>{
-        getCourses();
-    },[])
+  const getCourses = () => {
+    setIsLoading(true)
+    getData(`courses`).then((response) => {
+      if (response.success) {
+        setCourses(response?.data)
+        setIsLoading(false)
+      } else {
+        setSnackMsg(response.message);
+        setOpenSnack(true);
+        setIsLoading(false)
+      }
+    })
+      .catch((error) => {
+        setSnackMsg(error.message ?? "Network Error");
+        setOpenSnack(true);
+        setIsLoading(false)
+      });
+  }
 
-    // course creation handling 
+  useEffect(() => {
+    getCourses();
+  }, [])
+
+  // course creation handling 
   const [courseData, setCourseData] = useState({
     course_name: '',
     description: '',
@@ -82,9 +87,11 @@ const CourseManagement = () => {
           'Content-Type': 'multipart/form-data'
         }
       });
-      setMessage('Course created successfully');
-      setError('');
-      console.log('Course created successfully', response.data);
+      setIsLoading(false);
+      setSeverity('success')
+      setSnackMsg('Course Added Successfully.');
+      setOpenSnack(true);
+      getCourses();
       // Optionally, reset form fields here
       setCourseData({
         course_name: '',
@@ -94,65 +101,253 @@ const CourseManagement = () => {
       });
       setFiles([]);
     } catch (error) {
-      setMessage('');
-      setError('Error creating course. Please try again.');
-      console.error('Error creating course', error);
+      setIsLoading(false);
+      setSnackMsg('Error creating course. Please try again.')
+      setOpenSnack(true);
     }
   };
 
+  const editModal = () => {
+    return (
+      <Modal open={isEditing}>
+        <div className='ap-userModal-style' >
+          <div className='ap-userModal-content'>
+            <div
+              onClick={() => setIsEditing(false)}
+              className='ap-modal-cancel-icon'
+              ><CancelIcon /></div>
+            <Grid container spacing={5} >
+              <Grid item sm={6} xs={12}>
+                <TextField
+                  label="Course Name"
+                  variant='outlined'
+                  type="text"
+                  name="course_name"
+                  fullWidth
+                  value={courseData.course_name}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <TextField
+                  label="Instructor Name"
+                  variant='outlined'
+                  fullWidth
+                  type="text" name="instructor" value={courseData.instructor} onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Course Description"
+                  variant='outlined'
+                  fullWidth
+                  multiline
+                  name="description" value={courseData.description}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <TextField
+                  label="Course Duration"
+                  variant='outlined'
+                  fullWidth
+                  type="text" name="duration" value={courseData.duration} onChange={handleChange}
+                />
+              </Grid>
+              <Grid item sm={6} xs={12}>
+                <TextField
+                  // label="Course Duration"
+                  variant='outlined'
+                  fullWidth
+                  type="file" name="files" multiple onChange={handleFileChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <div className='text-center'>
+                  <Btn
+                    label='Save Course'
+                    onClick={handleEditSubmit}
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </Modal>
+
+    )
+  }
+
+  const handleEdit = (e) => {
+    setCourseData({
+      course_name: e?.course_name,
+      description: e?.description,
+      duration: e?.duration,
+      instructor: e?.instructor,
+      course_id: e?.course_id,
+    });
+    setFiles([...e.image_url]);
+    setIsEditing(true)
+  }
+
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('course_name', courseData.course_name);
+    formData.append('description', courseData.description);
+    formData.append('duration', courseData.duration);
+    formData.append('instructor', courseData.instructor);
+    for (let i = 0; i < files.length; i++) {
+      formData.append('files', files[i]);
+    }
+
+    try {
+      const response = await axios.put(`${API_BASE_URL}course/${courseData?.course_id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setIsLoading(false);
+      setSeverity('success')
+      setSnackMsg('Course Saved Successfully.');
+      setOpenSnack(true);
+      getCourses();
+      // Optionally, reset form fields here
+      setIsEditing(false)
+      setCourseData({
+        course_name: '',
+        description: '',
+        duration: '',
+        instructor: ''
+      });
+      setFiles([]);
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false);
+      setSnackMsg('Error in editing course. Please try again.')
+      setOpenSnack(true);
+    }
+  };
+
+  const handleDelete = (id) => {
+    setIsLoading(true)
+    deleteData(`/course/${id}`).then((response) => {
+        if (response.success) {
+            setSnackMsg(response.message );
+            setOpenSnack(true);
+            setSeverity('success');
+            getCourses();
+            setIsLoading(false);
+        } else {
+            setSnackMsg(response.error);
+            setOpenSnack(true);
+            setIsLoading(false);
+        }
+    })
+        .catch((error) => {
+            setSnackMsg(error.error ?? error.msg ?? "Network Error");
+            setOpenSnack(true);
+            setIsLoading(false);
+        });
+}
+
   return (
     <>
-    <div className="heading1" data-aos="fade-up">All Courses</div>
-    <div style={{margin:'20px 0px'}}>
-    <Grid container spacing={5}>
-        {courses && courses.length > 0 &&
-        courses.map((e,i)=>{
-            return(
-                <Grid item key={i} sm={4} xs={12}>
-                    <Card
+      {/* course creation form  */}
+      <div className="ap-upperMost">
+        <div className="dashboard-pd" />
+        <div>
+          <div
+            onClick={() => setIsAdding(!isAdding)}
+            className="ap-add-user-btn">Add Course</div>
+        </div>
+      </div>
+      {isAdding && <div>
+        <div className="heading2 text-center mb-20">Course Details</div>
+        {message && <p style={{ color: 'green' }}>{message}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <Grid container spacing={5} >
+          <Grid item sm={6} xs={12}>
+            <TextField
+              label="Course Name"
+              variant='outlined'
+              type="text"
+              name="course_name"
+              fullWidth
+              value={courseData.course_name}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <TextField
+              label="Instructor Name"
+              variant='outlined'
+              fullWidth
+              type="text" name="instructor" value={courseData.instructor} onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Course Description"
+              variant='outlined'
+              fullWidth
+              multiline
+              name="description" value={courseData.description}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <TextField
+              label="Course Duration"
+              variant='outlined'
+              fullWidth
+              type="text" name="duration" value={courseData.duration} onChange={handleChange}
+            />
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <TextField
+              // label="Course Duration"
+              variant='outlined'
+              fullWidth
+              type="file" name="files" multiple onChange={handleFileChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <div className='text-center'>
+              <Btn
+                label='Create Course'
+                onClick={handleSubmit}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </div>}
+      <div className="heading2 mb-40" >All Courses</div>
+      <div style={{ margin: '20px 0px' }}>
+        <Grid container spacing={5}>
+          {courses && courses.length > 0 &&
+            courses.map((e, i) => {
+              return (
+                <Grid item key={i} md={4} sm={6} xs={12}>
+                  <Card
                     name={e?.course_name}
                     description={e?.description}
                     duration={e?.duaration}
-                    // img={e?.image_url}
-                    />
-                 </Grid>   
-            )
-        })
-        }
-    </Grid>
-    </div>
-                    
-    {/* course creation form  */}
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Course Name:</label>
-          <input type="text" name="course_name" style={{border:'1px solid black'}} value={courseData.course_name} onChange={handleChange} />
-        </div>
-        <br />
-        <div>
-          <label>Description:</label>
-          <textarea name="description" value={courseData.description} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Duration:</label>
-          <input type="text" name="duration" value={courseData.duration} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Instructor:</label>
-          <input type="text" name="instructor" value={courseData.instructor} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Files:</label>
-          <input type="file" name="files" multiple onChange={handleFileChange} />
-        </div>
-        <button type="submit">Create Course</button>
-      </form>
-      {message && <p style={{ color: 'green' }}>{message}</p>}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-    </div>
-    <Snack msg={snackMsg} open={openSnack} onClose={handleCloseSnack} severity={severity} />
-    <Loader isLoading={isLoading} />
+                    showControls={true}
+                    onEdit={() => handleEdit(e)}
+                    onDelete={()=>handleDelete(e?.course_id)}
+                  // img={e?.image_url}
+                  />
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+      </div>
+
+      {editModal()}
+      <Snack msg={snackMsg} open={openSnack} onClose={handleCloseSnack} severity={severity} />
+      <Loader isLoading={isLoading} />
     </>
   );
 };
