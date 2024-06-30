8358d9fd609ad80f1757bc9bb9d10106076a717f
Noor1236 <slaveofnoor@gmail.com>
2024-06-30 05:50:35 +0500

    Discard the changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-// index.js
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Adjust the path as per your project structure
-import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
